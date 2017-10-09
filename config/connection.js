7339680c4e38111f2728d8228b3f05e53e7a1a46
Vaijayanthi Neelakandan <nvaij@yahoo.com>
2017-10-09 10:43:56 -0400

    added jawsdb

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,16 @@
 var mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "UNCCamp448",
-  database: "burgers_db"
-});
+if(process.env.JAWSDB_URL){
+	connection = mysql.createConnection(process.env.JAWSDB_URL)
+} else {
+	 connection = mysql.createConnection({
+	  host: "localhost",
+	  user: "root",
+	  password: "UNCCamp448",
+	  database: "burgers_db"
+	});
+}
 
 connection.connect(function(err) {
   if (err) {
